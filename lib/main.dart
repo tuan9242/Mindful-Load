b017e881d71d50f0712ad13ff76dadeb5616351c
tuan9242 <tuanvuanh103@gmail.com>
2026-01-06 11:23:53 +0700

    [Feature] tao giao dien phan tich chi tiet

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mindful_load/features/news/screens/analysis_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const AnalysisScreen(),
     );
   }
 }
